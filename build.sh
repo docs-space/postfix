8ab35c4c3cab053e07d2f844b90cdb3068d47b99
Ilya Talipov <talipov.ilya@r7-office.ru>
2025-12-12 19:27:18 +0300

    change build script

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+VERSION=$1
+BUILD_DIRECTORY=/opt/r7-mailserver/mtaserver
+PACK_DIRECTORY=$2/Pack/opt/r7-mailserver/
 
-
-Здесь будет скрипт сборки postfix из исходников
+echo "Здесь будет скрипт сборки postfix из исходников"
