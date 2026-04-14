c6d3f23e45d250bafbbdc3c3a2507fea63999f29
Ilya Talipov <talipov.ilya@r7-office.ru>
2026-04-14 21:05:12 +0300

    fix error build: du: cannot access '/opt/buildagent/work/d9641f8e06ca11b/Pack/src': No such file or directory

--- build_package.sh
+++ build_package.sh
@@ -6,7 +6,7 @@ DIR_FILES=$2
 #Prepare
 sed -i "s/Version:.*/Version: ${VERSION}/" $DIR_FILES/Pack/DEBIAN/control
 chmod -R 755 $DIR_FILES/Pack/DEBIAN
-sed -i "s/Installed-Size:.*/Installed-Size: $(du -sk $DIR_FILES/Pack/src | cut -f1)/" $DIR_FILES/Pack/src/DEBIAN/control
+sed -i "s/Installed-Size:.*/Installed-Size: $(du -sk $DIR_FILES/Pack | cut -f1)/" $DIR_FILES/Pack/DEBIAN/control
 ( cd "$DIR_FILES/Pack" && rm -f DEBIAN/md5sums && find . -type f ! -path '*/DEBIAN/*' -exec md5sum {} > DEBIAN/md5sums \; )
 
 #Build
