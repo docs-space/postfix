b3ba6d9201aa0f6575b74e39fc59389a63032748
Ilya Talipov <talipov.ilya@r7-office.ru>
2026-04-14 20:57:54 +0300

    clearing build package scripts

--- build_package.sh
+++ build_package.sh
@@ -4,16 +4,11 @@ DIR_FILES=$2
 
 
 #Prepare
-sudo sed -i "s/Version:.*/Version: ${VERSION}/" $DIR_FILES/Pack/DEBIAN/control
-sudo chmod -R 755 $DIR_FILES/Pack/DEBIAN
-size_inKb=$(du -sk $DIR_FILES/Pack | cut -f1)
-sudo sed -i "s/Installed-Size:.*/Installed-Size: ${size_inKb}/" $DIR_FILES/Pack/DEBIAN/control
+sed -i "s/Version:.*/Version: ${VERSION}/" $DIR_FILES/Pack/DEBIAN/control
+chmod -R 755 $DIR_FILES/Pack/DEBIAN
+sed -i "s/Installed-Size:.*/Installed-Size: $(du -sk $DIR_FILES/Pack/src | cut -f1)/" $DIR_FILES/Pack/src/DEBIAN/control
+( cd "$DIR_FILES/Pack" && rm -f DEBIAN/md5sums && find . -type f ! -path '*/DEBIAN/*' -exec md5sum {} > DEBIAN/md5sums \; )
 
-NOWDIR=$PWD
-cd $DIR_FILES/Pack
-find . -type f ! -path '*/DEBIAN/*' -exec md5sum {} > DEBIAN/md5sums \;
-cd $NOWDIR
 #Build
-sudo dpkg-deb -b $DIR_FILES/Pack $DIR_FILES/Packages/r7mtaserver_${VERSION}.deb
-rm -f $DIR_FILES/Pack/DEBIAN/md5sums
-sudo md5sum $DIR_FILES/Packages/r7mtaserver_${VERSION}.deb > $DIR_FILES/Packages/md5.txt
+dpkg-deb -b $DIR_FILES/Pack $DIR_FILES/Packages/r7mtaserver_${VERSION}.deb
+md5sum $DIR_FILES/Packages/r7mtaserver_${VERSION}.deb > $DIR_FILES/Packages/md5.txt
